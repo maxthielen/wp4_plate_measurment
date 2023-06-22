f021035528bf0e0af7dbabfd4b39d39194314c93
Steven Visch <visch136@gmail.com>
2023-06-22 15:38:56 +0200

    cahgens

--- dev_build_image.sh
+++ dev_build_image.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 echo "Building development image for the plate measurement demonstrator"
-docker build . -f dev.Dockerfile -t visir/plate_measurement_demonstrator_dev
+docker build . --no-cache -f dev.Dockerfile -t visir/plate_measurement_demonstrator_dev
